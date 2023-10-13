ae6fcd93b7185d4ab2c8c48c64f4a41a5d78b4d0
Denzel Huijbers <huijbers15@outlook.com>
2023-10-13 11:53:23 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using VisitorPlacementLogic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var table = document.getElementById("visitor-table");
+
+
+document.addEventListener("DOMContentLoaded"){
+    CheckPresentVisitors(){ }
+}
+function CheckPresentVisitors() {
+    var visitors = @Model.Visitors;
+        
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            var tableEntry = (row.children[0].textContent)
+
+            console.log(tableEntry, visitor.Name)
+        }
+    }
+
+
+
